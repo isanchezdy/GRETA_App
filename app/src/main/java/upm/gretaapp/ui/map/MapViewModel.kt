ba482cc3d8bd9965689674b24a62b18b4198764c
isanchezdy <isanchezdy@alumnos.unex.es>
2024-04-02 16:10:30 +0200

    Updated methods documentation

--- app/src/main/java/upm/gretaapp/ui/map/MapViewModel.kt
+++ app/src/main/java/upm/gretaapp/ui/map/MapViewModel.kt
@@ -14,7 +14,6 @@ import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.lastOrNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.job
